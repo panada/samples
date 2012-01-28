1a35a445488d44aa54f9eee0ec8955518e79bac8
k4ndar <k4ndar@yahoo.com>
2012-01-28 14:07:15 +0700

    add db port in config database

--- simple-login/config/database.php
+++ simple-login/config/database.php
@@ -5,6 +5,7 @@ return array(
     'default' => array(
         'driver' => 'mysql',
         'host' => 'localhost',
+        'port' => 3306,
         'user' => 'root',
         'password' => '',
         'database' => 'panada',
